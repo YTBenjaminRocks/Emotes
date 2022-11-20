81334f65043e82307bc763921ac701f247b0d3ad
peterwalcker <87015108+peterwalcker@users.noreply.github.com>
2022-11-20 22:09:21 +0300

    qr test

--- service/QRLinks.js
+++ service/QRLinks.js
@@ -1,6 +1,17 @@
 let counter = 0;
-
 let timerid = setInterval(changeQR, 30000);
+let arr = 
+	["QR_Discord.png",
+	"QR_Youtube.png"];
+
+let div = document.getElementById("qr_div");
+for (int i = 0; i < arr.length; i++) {
+	let img = document.createElement("img");
+	img.className = "qr";
+	img.hidden = true;
+	img.src = "service/" + arr[i];
+	div.appendChild(img);
+}
 
 function changeQR() {
     let QR = document.getElementsByClassName("qr");
